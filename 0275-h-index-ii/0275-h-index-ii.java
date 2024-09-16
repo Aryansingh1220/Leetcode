ef36f56cc8cdda8bd30e3a085dde39a0d01751d4
Aryan singh <120249017+Aryansingh1220@users.noreply.github.com>
2024-09-17 01:21:52 +0530

    Time: 1 ms (27.17%), Space: 47.8 MB (52.26%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.java
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     public int hIndex(int[] citations) {
-        Arrays.sort(citations);
+        // Arrays.sort(citations);
         int n=citations.length;
         for(int i=0;i<n;i++){
             int h=n-i;
