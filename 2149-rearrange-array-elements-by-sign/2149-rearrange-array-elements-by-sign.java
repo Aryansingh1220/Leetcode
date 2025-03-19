73d7b2a024321cbdd0c2da106e3973d6848b6636
Aryan singh <120249017+Aryansingh1220@users.noreply.github.com>
2025-03-19 13:29:23 +0530

    Time: 15 ms (5.5%), Space: 86.1 MB (5.74%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        ArrayList<Integer>list1=new ArrayList<>();
+        ArrayList<Integer>list2=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>=0){
+                list1.add(nums[i]);
+            }
+            else{
+                list2.add(nums[i]);
+            }
+        }
+        int[] ans=new int[nums.length];
+        ArrayList<Integer>temp=new ArrayList<>();
+        Collections.fill(temp, 0);
+        int a=0;
+        int b=0;
+        for(int i=0;i<nums.length;i++){
+            if(i%2==0){
+                temp.add(list1.get(a));
+                a++;
+            }
+            else{
+                temp.add(list2.get(b));
+                b++;
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+            ans[i]=temp.get(i);
+        }
+        return ans;
+    }
+}
